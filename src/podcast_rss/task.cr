83336c59271981910c7977403618f5dafc922253
lost <lost>
2024-04-22 00:49:12 +0800

    fix

--- src/podcast_rss/task.cr
+++ src/podcast_rss/task.cr
@@ -9,7 +9,7 @@ class PodcastRss::ChannelSyncTask
     last_channel_item = PodcastRss::Repo.get_last_channel_item(@channel.rss)
 
     # fetching
-    fetch_channel_by last_channel_item
+    channel = fetch_channel_by last_channel_item
 
     # saving
     PodcastRss.update_channel channel
@@ -17,12 +17,12 @@ class PodcastRss::ChannelSyncTask
   end
 
   def fetch_channel_by(last_channel_item : PodcastRss::ChannelItem | Nil) : PodcastRss::Channel
-    rss_xml = PodcastRss.get_rss_xml_io(channel.rss)
+    rss_xml = PodcastRss.get_rss_xml_io(@channel.rss)
     if last_channel_item
       # parsing incremental items
       PodcastRss.parse_channel rss_xml do |channel|
         need_stop = (channel.items.last.title == last_channel_item.title)
-        channel.items.remove_last if need_stop
+        channel.items.pop if need_stop
         need_stop
       end
     else
