a109b78635a541551f3a2901f6227ce01e1d0964
lost <lost>
2024-04-22 00:33:44 +0800

    make test eazier

--- src/podcast_rss/task.cr
+++ src/podcast_rss/task.cr
@@ -9,21 +9,25 @@ class PodcastRss::ChannelSyncTask
     last_channel_item = PodcastRss::Repo.get_last_channel_item(@channel.rss)
 
     # fetching
-    rss_xml = PodcastRss.get_rss_xml_io(channel.rss)
-    channel = if last_channel_item
-                # parsing incremental items
-                PodcastRss.parse_channel rss_xml do |channel|
-                  need_stop = (channel.items.last.title == last_channel_item.title)
-                  channel.items.remove_last if need_stop
-                  need_stop
-                end
-              else
-                # parsing all items
-                PodcastRss.parse_channel rss_xml
-              end
+    fetch_channel_by last_channel_item
 
     # saving
     PodcastRss.update_channel channel
     PodcastRss.add_channel_items(channel.items)
+  end
+
+  def fetch_channel_by(last_channel_item : PodcastRss::ChannelItem | Nil) : PodcastRss::Channel
+    rss_xml = PodcastRss.get_rss_xml_io(channel.rss)
+    if last_channel_item
+      # parsing incremental items
+      PodcastRss.parse_channel rss_xml do |channel|
+        need_stop = (channel.items.last.title == last_channel_item.title)
+        channel.items.remove_last if need_stop
+        need_stop
+      end
+    else
+      # parsing all items
+      PodcastRss.parse_channel rss_xml
+    end
   end
 end
