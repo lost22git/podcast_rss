f56641024b9d4f4fee725d11e9b6f981490b8f20
lost <lost>
2024-04-22 10:20:59 +0800

    update

--- src/podcast_rss.cr
+++ src/podcast_rss.cr
@@ -76,16 +76,3 @@ module PodcastRss
     PodcastRss::Repo.get_latest_channel_items channel_id, size
   end
 end
-
-rss_list = [
-  "http://rss.lizhi.fm/rss/14275.xml",
-  "http://www.ximalaya.com/album/4494083.xml",
-  "https://s1.proxy.wavpub.com/storyhunting.xml",
-]
-
-rss_list.each do |rss_link|
-  PodcastRss.get_rss_xml_io(rss_link) do |rss_xml|
-    channel = PodcastRss.parse_channel rss_xml
-    channel.print
-  end
-end
