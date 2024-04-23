ebd179c0a176f80b3e6e95d8d1616597dc6e34ba
lost <lost>
2024-04-23 20:30:33 +0800

    fix

--- init.sql
+++ init.sql
@@ -1,27 +1,28 @@
 DROP TABLE IF EXISTS channel;
 CREATE TABLE channel (
-	id VARCHAR NOT NULL,
-  rss VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
-	description VARCHAR,
-  author VARCHAR,
-	image VARCHAR,
-	"language" VARCHAR,
-	CONSTRAINT CHANNEL_PK PRIMARY KEY (id)
+    id VARCHAR NOT NULL,
+    rss VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    description VARCHAR,
+    author VARCHAR,
+    image VARCHAR,
+    "language" VARCHAR,
+    CONSTRAINT channel_pk PRIMARY KEY (id)
 );
 
 
 DROP TABLE IF EXISTS channel_item;
 CREATE TABLE channel_item (
-  id VARCHAR NOT NULL,
-  channel_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
-	subtitle VARCHAR,
-	description VARCHAR,
-	image VARCHAR,
-	duration VARCHAR,
-	url VARCHAR,
-	"type" VARCHAR,
-	"length" VARCHAR,
-	CONSTRAINT CHANNEL_ITEM_PK PRIMARY KEY (id)
+    id VARCHAR NOT NULL,
+    channel_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    subtitle VARCHAR,
+    description VARCHAR,
+    pub_date VARCHAR,
+    image VARCHAR,
+    duration VARCHAR,
+    url VARCHAR,
+    "type" VARCHAR,
+    "length" VARCHAR,
+    CONSTRAINT channel_item_pk PRIMARY KEY (id)
 );
